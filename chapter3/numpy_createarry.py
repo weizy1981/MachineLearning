31a0b653dde3d8b63f309e80f22eb445ec1f840f
weizy1981 <zhenyuanwei@foxmail.com>
2017-06-19 10:42:46 +0800

    init

---
+++ chapter3/numpy_createarry.py
@@ -0,0 +1,9 @@
+import numpy as np
+# 创建数组
+myarray = np.array([1, 2, 3])
+print(myarray)
+print(myarray.shape)
+#创建多维数组
+myarray = np.array([[1, 2, 3], [2, 3, 4], [3, 4, 5]])
+print(myarray)
+print(myarray.shape)
