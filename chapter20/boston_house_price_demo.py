511864caa951c588b38ead337a39e6e65bbed2ff
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-07-09 21:21:18 +0800

    init comments

---
+++ chapter20/boston_house_price_demo.py
@@ -0,0 +1,30 @@
+# 导入类库
+import numpy as np
+from numpy import arange
+from matplotlib import pyplot
+from pandas import read_csv
+from pandas import  set_option
+from pandas.plotting import scatter_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.metrics import mean_squared_error
+
+# 导入数据
+filename = 'housing.csv'
+names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS',
+         'RAD', 'TAX', 'PRTATIO', 'B', 'LSTAT', 'MEDV']
+data = read_csv(filename, names=names, delim_whitespace=True)
