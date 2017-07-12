ea9700feb761dea29e0dc32c02cb5567e8f0b10e
zhenyuanwei <zhenyuanwei@foxmail.com>
2017-07-13 07:31:45 +0800

    init comments

--- chapter12/svm.py
+++ chapter12/svm.py
@@ -1,20 +1,18 @@
 from pandas import read_csv
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
-from sklearn.svm import SVR
+from sklearn.svm import SVC
 # 导入数据
-filename = 'housing.csv'
-names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS',
-         'RAD', 'TAX', 'PRTATIO', 'B', 'LSTAT', 'MEDV']
-data = read_csv(filename, names=names, delim_whitespace=True)
+filename = 'pima_data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(filename, names=names)
 # 将数据分为输入数据和输出结果
 array = data.values
-X = array[:, 0:13]
-Y = array[:, 13]
-n_splits = 10
+X = array[:, 0:8]
+Y = array[:, 8]
+num_folds = 10
 seed = 7
-kfold = KFold(n_splits=n_splits, random_state=seed)
-model = SVR()
-scoring = 'neg_mean_squared_error'
-result = cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
-print('SVM: %.3f' % result.mean())
+kfold = KFold(n_splits=num_folds, random_state=seed)
+model = SVC()
+result = cross_val_score(model, X, Y, cv=kfold)
+print(result.mean())
--- chapter20/boston_house_price_demo.py
+++ chapter20/boston_house_price_demo.py
@@ -28,7 +28,7 @@ filename = 'housing.csv'
 names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS',
          'RAD', 'TAX', 'PRTATIO', 'B', 'LSTAT', 'MEDV']
 dataset = read_csv(filename, names=names, delim_whitespace=True)
-
+'''
 # 数据维度
 print(dataset.shape)
 
@@ -38,3 +38,125 @@ print(dataset.dtypes)
 # 查看最开始的30条记录
 set_option('display.line_width', 120)
 print(dataset.head(30))
+
+# 描述性统计信息
+set_option('precision', 1)
+print(dataset.describe())
+
+# 关联关系
+set_option('precision', 2)
+print(dataset.corr(method='pearson'))
+
+# 直方图
+dataset.hist(sharex=False, sharey=False, xlabelsize=1, ylabelsize=1)
+pyplot.show()
+
+# 密度图
+dataset.plot(kind='density', subplots=True, layout=(4,4), sharex=False, fontsize=1)
+pyplot.show()
+
+# 箱线图
+dataset.plot(kind='box', subplots=True, layout=(4,4), sharex=False, sharey=False, fontsize=8)
+pyplot.show()
+
+# 散点矩阵图
+scatter_matrix(dataset)
+pyplot.show()
+
+# 相关矩阵图
+fig = pyplot.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(dataset.corr(), vmin=-1, vmax=1, interpolation='none')
+fig.colorbar(cax)
+ticks = np.arange(0, 14, 1)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.set_xticklabels(names)
+ax.set_yticklabels(names)
+pyplot.show()
+'''
+# 分离数据集
+array = dataset.values
+X = array[:, 0:13]
+Y = array[:, 13]
+validation_size = 0.2
+seed = 7
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y,test_size=validation_size, random_state=seed)
+
+# 评估算法 - 评估标准
+num_folds = 10
+seed = 7
+scoring = 'neg_mean_squared_error'
+'''
+# 评估算法 - baseline
+models = {}
+models['LR'] = LinearRegression()
+models['LASSO'] = Lasso()
+models['EN'] = ElasticNet()
+models['KNN'] = KNeighborsRegressor()
+models['CART'] = DecisionTreeRegressor()
+models['SVM'] = SVR()
+# 评估算法
+results = []
+for key in models:
+    kfold = KFold(n_splits=num_folds, random_state=seed)
+    cv_result = cross_val_score(models[key], X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_result)
+    print('%s: %f (%f)' % (key, cv_result.mean(), cv_result.std()))
+#评估算法 - 箱线图
+fig = pyplot.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(models.keys())
+pyplot.show()
+
+# 评估算法 - 正态化数据
+pipelines = {}
+pipelines['ScalerLR'] = Pipeline([('Scaler', StandardScaler()), ('LR', LinearRegression())])
+pipelines['ScalerLASSO'] = Pipeline([('Scaler', StandardScaler()), ('LASSO', Lasso())])
+pipelines['ScalerEN'] = Pipeline([('Scaler', StandardScaler()), ('EN', ElasticNet())])
+pipelines['ScalerKNN'] = Pipeline([('Scaler', StandardScaler()), ('KNN', KNeighborsRegressor())])
+pipelines['ScalerCART'] = Pipeline([('Scaler', StandardScaler()), ('CART', DecisionTreeRegressor())])
+pipelines['ScalerSVM'] = Pipeline([('Scaler', StandardScaler()), ('SVM', SVR())])
+results = []
+for key in pipelines:
+    kfold = KFold(n_splits=num_folds, random_state=seed)
+    cv_result = cross_val_score(pipelines[key], X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_result)
+    print('%s: %f (%f)' % (key, cv_result.mean(), cv_result.std()))
+#评估算法 - 箱线图
+fig = pyplot.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+pyplot.boxplot(results)
+ax.set_xticklabels(models.keys())
+pyplot.show()
+
+# 调参改进算法 - KNN
+scaler = StandardScaler().fit(X_train)
+rescaledX = scaler.transform(X_train)
+param_grid = {'n_neighbors': [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21]}
+model = KNeighborsRegressor()
+kfold = KFold(n_splits=num_folds, random_state=seed)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=scoring, cv=kfold)
+grid_result = grid.fit(X=rescaledX, y=Y_train)
+
+print('最优：%s 使用%s' % (grid_result.best_score_, grid_result.best_params_))
+cv_results = zip(grid_result.cv_results_['mean_test_score'],
+                 grid_result.cv_results_['std_test_score'],
+                 grid_result.cv_results_['params'])
+for mean, std, param in cv_results:
+    print('%f (%f) with %r' % (mean, std, param))
+'''
+# 集成算法
+ensembles = {}
+ensembles['ScaledAB'] = Pipeline([('Scaler', StandardScaler()), ('AB', AdaBoostRegressor())])
+ensembles['ScaledAB-KNN'] = Pipeline([('Scaler', StandardScaler()),
+                                       ('AB', AdaBoostRegressor(base_estimator=KNeighborsRegressor(n_neighbors=3)))])
+results = []
+for key in ensembles:
+    kfold = KFold(n_splits=num_folds, random_state=seed)
+    cv_result = cross_val_score(ensembles[key], X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_result)
+    print('%s: %f (%f)' % (key, cv_result.mean(), cv_result.std()))
